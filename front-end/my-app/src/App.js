b74c0a86ee836be3f0f1325621c0eab7ce861e77
Brando felipe cortes <brandoncortes81@gmail.com>
2022-11-14 15:03:36 -0500

    dia3

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateUser from "./component/account";
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <Route path="/" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
